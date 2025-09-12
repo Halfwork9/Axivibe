9f08e90aae251288f9d5ab5479895ea4f5e15684
Halfwork9 <halfworks9@gmail.com>
2025-09-12 11:35:50 +0530

    Update index.js

--- src/store/admin/products-slice/index.js
+++ src/store/admin/products-slice/index.js
@@ -1,5 +1,6 @@
+// store/admin/index.js
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
+import api from "@/api";
 
 const initialState = {
   isLoading: false,
@@ -9,16 +10,7 @@ const initialState = {
 export const addNewProduct = createAsyncThunk(
   "/products/addnewproduct",
   async (formData) => {
-    const result = await axios.post(
-      "http://localhost:5000/api/admin/products/add",
-      formData,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
+    const result = await api.post("/admin/products/add", formData);
     return result?.data;
   }
 );
@@ -26,10 +18,7 @@ export const addNewProduct = createAsyncThunk(
 export const fetchAllProducts = createAsyncThunk(
   "/products/fetchAllProducts",
   async () => {
-    const result = await axios.get(
-      "http://localhost:5000/api/admin/products/get"
-    );
-
+    const result = await api.get("/admin/products/get");
     return result?.data;
   }
 );
@@ -37,16 +26,7 @@ export const fetchAllProducts = createAsyncThunk(
 export const editProduct = createAsyncThunk(
   "/products/editProduct",
   async ({ id, formData }) => {
-    const result = await axios.put(
-      `http://localhost:5000/api/admin/products/edit/${id}`,
-      formData,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
+    const result = await api.put(`/admin/products/edit/${id}`, formData);
     return result?.data;
   }
 );
@@ -54,10 +34,7 @@ export const editProduct = createAsyncThunk(
 export const deleteProduct = createAsyncThunk(
   "/products/deleteProduct",
   async (id) => {
-    const result = await axios.delete(
-      `http://localhost:5000/api/admin/products/delete/${id}`
-    );
-
+    const result = await api.delete(`/admin/products/delete/${id}`);
     return result?.data;
   }
 );
