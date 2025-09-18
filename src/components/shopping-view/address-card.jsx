ed9e85ba6bb748ebad9ff31d729043c76775be47
Halfwork9 <halfworks9@gmail.com>
2025-09-18 12:02:26 +0530

    Update address-card.jsx

--- src/components/shopping-view/address-card.jsx
+++ src/components/shopping-view/address-card.jsx
@@ -28,7 +28,7 @@ function AddressCard({
         <Label>City: {addressInfo?.city}</Label>
         <Label>Pincode: {addressInfo?.pincode}</Label>
         <Label>Phone: {addressInfo?.phone}</Label>
-        <Label>Notes: {addressInfo?.notes}</Label>
+    //    <Label>Notes: {addressInfo?.notes}</Label>
       </CardContent>
       <CardFooter className="p-3 flex justify-between">
         <Button onClick={() => handleEditAddress(addressInfo)}>Edit</Button>
