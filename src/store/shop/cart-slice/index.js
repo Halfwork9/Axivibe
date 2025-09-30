51fc4edb714530bc68b89bfc2988dfe29b1d660c
Halfwork9 <halfworks9@gmail.com>
2025-09-30 11:47:34 +0530

    Update index.js

--- src/store/shop/cart-slice/index.js
+++ src/store/shop/cart-slice/index.js
@@ -70,49 +70,34 @@ const shoppingCartSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      .addCase(addToCart.pending, (state) => {
-        state.isLoading = true;
-      })
+      .addCase(addToCart.pending, (state) => { state.isLoading = true; })
       .addCase(addToCart.fulfilled, (state, action) => {
         state.isLoading = false;
-        state.cartItems = action.payload.data;
+        state.cartItems = action.payload.data.items || [];
       })
       .addCase(addToCart.rejected, (state) => {
         state.isLoading = false;
         state.cartItems = [];
       })
-      .addCase(fetchCartItems.pending, (state) => {
-        state.isLoading = true;
-      })
+
+      .addCase(fetchCartItems.pending, (state) => { state.isLoading = true; })
       .addCase(fetchCartItems.fulfilled, (state, action) => {
         state.isLoading = false;
-        state.cartItems = action.payload.data;
+        state.cartItems = action.payload.data.items || [];
       })
       .addCase(fetchCartItems.rejected, (state) => {
         state.isLoading = false;
         state.cartItems = [];
       })
-      .addCase(updateCartQuantity.pending, (state) => {
-        state.isLoading = true;
-      })
+
       .addCase(updateCartQuantity.fulfilled, (state, action) => {
         state.isLoading = false;
-        state.cartItems = action.payload.data;
+        state.cartItems = action.payload.data.items || [];
       })
-      .addCase(updateCartQuantity.rejected, (state) => {
-        state.isLoading = false;
-        state.cartItems = [];
-      })
-      .addCase(deleteCartItem.pending, (state) => {
-        state.isLoading = true;
-      })
+
       .addCase(deleteCartItem.fulfilled, (state, action) => {
         state.isLoading = false;
-        state.cartItems = action.payload.data;
-      })
-      .addCase(deleteCartItem.rejected, (state) => {
-        state.isLoading = false;
-        state.cartItems = [];
+        state.cartItems = action.payload.data.items || [];
       });
   },
 });
