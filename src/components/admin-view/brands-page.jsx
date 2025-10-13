2384ff94d92525a22298ed245272a6aca51e438a
Halfwork9 <halfworks9@gmail.com>
2025-10-13 17:42:39 +0530

    Update brands-page.jsx

--- src/components/admin-view/brands-page.jsx
+++ src/components/admin-view/brands-page.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { createBrand, deleteBrand, fetchAllBrands, editBrand } from "@/store/admin/brand-slice";
-import { Card, CardContent, CardFooter } from "../../../components/ui/card";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -186,6 +186,7 @@ function AdminBrandsPage() {
 }
 
 export default AdminBrandsPage;
+
 
 
 
