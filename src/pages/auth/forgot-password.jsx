c2629104d60664af9de8f9bc5b13e7674e5f8bf0
Halfwork9 <halfworks9@gmail.com>
2025-10-18 13:49:06 +0530

    Update forgot-password.jsx

--- src/pages/auth/forgot-password.jsx
+++ src/pages/auth/forgot-password.jsx
@@ -1,10 +1,11 @@
+
 import { useState } from "react";
-import { Button } from "../../components/ui/button";
-import { Input } from "../../components/ui/input";
-import { Label } from "../../components/ui/label";
-import { useToast } from "../../components/ui/use-toast";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useToast } from "@/components/ui/use-toast";
 import { Link } from "react-router-dom";
-import api from "../../api";
+import api from "@/api";
 
 function ForgotPassword() {
   const [email, setEmail] = useState("");
