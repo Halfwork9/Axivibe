98542d63b0c8c14c247e489cb8fd79f805d29d5d
Halfwork9 <halfworks9@gmail.com>
2025-10-18 13:57:10 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,27 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "@/App.jsx";
+import "@/index.css";
 import { Provider } from "react-redux";
-import store from "./store/store.js";
-import { Toaster } from "./components/ui/toaster.jsx";
+import store from "@/store/index.js";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { HelmetProvider } from "react-helmet-async";
+import { Toaster } from "@/components/ui/toaster.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <HelmetProvider>
-      <App />
-      <Toaster />
-      </HelmetProvider>
-    </Provider>
-  </BrowserRouter>
+// Ensure you have your Google Client ID in your .env file
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* ✅ FIX: Wrap the entire application with GoogleOAuthProvider */}
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <Provider store={store}>
+        <HelmetProvider>
+          <App />
+          <Toaster />
+        </HelmetProvider>
+      </Provider>
+    </GoogleOAuthProvider>
+  </React.StrictMode>
 );
+
