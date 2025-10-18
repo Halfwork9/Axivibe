79d70de08044609bb6e90aee187827f6b5bf4e05
Halfwork9 <halfworks9@gmail.com>
2025-10-18 14:42:18 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "@/App.jsx";
 import "@/index.css";
 import { Provider } from "react-redux";
-import store from "@/store/index.js";
+import store from "./store/store.js";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { HelmetProvider } from "react-helmet-async";
 import { Toaster } from "@/components/ui/toaster.jsx";
