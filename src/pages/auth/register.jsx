3e4c8196e235c6f2a6e0ee4f54505e82ecd89d7c
Halfwork9 <halfworks9@gmail.com>
2025-10-18 13:29:28 +0530

    Update register.jsx

--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -6,49 +6,33 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 
-const initialState = {
-  userName: "",
-  email: "",
-  password: "",
-};
-
 function AuthRegister() {
-  const [formData, setFormData] = useState(initialState);
+  const [formData, setFormData] = useState({ userName: "", email: "", password: "" });
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { toast } = useToast();
 
-  function onSubmit(event) {
+  const onSubmit = (event) => {
     event.preventDefault();
     dispatch(registerUser(formData)).then((data) => {
       if (data?.payload?.success) {
-        toast({
-          title: data?.payload?.message,
-        });
+        toast({ title: data?.payload?.message });
         navigate("/auth/login");
       } else {
-        toast({
-          title: data?.payload?.message,
-          variant: "destructive",
-        });
+        toast({ title: data?.payload?.message, variant: "destructive" });
       }
     });
-  }
-
-  console.log(formData);
+  };
 
   return (
-    <div className="mx-auto w-full max-w-md space-y-6">
+    <div className="mx-auto w-full max-w-sm space-y-6 p-8 rounded-xl shadow-lg bg-white">
       <div className="text-center">
-        <h1 className="text-3xl font-bold tracking-tight text-foreground">
-          Create new account
+        <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+          Create an Account
         </h1>
-        <p className="mt-2">
-          Already have an account
-          <Link
-            className="font-medium ml-2 text-primary hover:underline"
-            to="/auth/login"
-          >
+        <p className="mt-2 text-gray-600">
+          Already have an account?{" "}
+          <Link className="font-medium text-primary hover:underline" to="/auth/login">
             Login
           </Link>
         </p>
@@ -65,3 +49,4 @@ function AuthRegister() {
 }
 
 export default AuthRegister;
+
