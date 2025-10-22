3484f91bdf29a305a6c47c141194ef4a81fd96b4
Halfwork9 <halfworks9@gmail.com>
2025-10-22 16:43:48 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,42 +1,12 @@
-export const registerFormControls = [
-  {
-    name: "userName",
-    label: "User Name",
-    placeholder: "Enter your user name",
-    componentType: "input",
-    type: "text",
-  },
-  {
-    name: "email",
-    label: "Email",
-    placeholder: "Enter your email",
-    componentType: "input",
-    type: "email",
-  },
-  {
-    name: "password",
-    label: "Password",
-    placeholder: "Enter your password",
-    componentType: "input",
-    type: "password",
-  },
+export const loginFormControls = [
+  { name: 'email', type: 'email', label: 'Email', placeholder: 'Enter your email' },
+  { name: 'password', type: 'password', label: 'Password', placeholder: 'Enter your password' },
 ];
 
-export const loginFormControls = [
-  {
-    name: "email",
-    label: "Email",
-    placeholder: "Enter your email",
-    componentType: "input",
-    type: "email",
-  },
-  {
-    name: "password",
-    label: "Password",
-    placeholder: "Enter your password",
-    componentType: "input",
-    type: "password",
-  },
+export const registerFormControls = [
+  { name: 'userName', type: 'text', label: 'Username', placeholder: 'Enter your username' },
+  { name: 'email', type: 'email', label: 'Email', placeholder: 'Enter your email' },
+  { name: 'password', type: 'password', label: 'Password', placeholder: 'Enter your password' },
 ];
 
 export const addProductFormElements = [
