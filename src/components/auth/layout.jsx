dcc86a24a9fa91cf3b2390d915c2447d7d65493e
Halfwork9 <halfworks9@gmail.com>
2025-10-23 09:58:10 +0530

    Update layout.jsx

--- src/components/auth/layout.jsx
+++ src/components/auth/layout.jsx
@@ -1,18 +1,13 @@
+// src/components/auth/layout.jsx
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
-const AuthLayout = () => {
-  console.log('AuthLayout: Rendering');
-  try {
-    return (
-      <div className="flex min-h-screen bg-gray-50">
-        <Outlet />
-      </div>
-    );
-  } catch (err) {
-    console.error('AuthLayout: Render error:', err);
-    throw err;
-  }
-};
+function AuthLayout() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <Outlet />
+    </div>
+  );
+}
 
 export default AuthLayout;
