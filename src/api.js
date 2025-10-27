1394e67875a30af4f3787b780a46202b43d75e27
Halfwork9 <halfworks9@gmail.com>
2025-10-27 17:25:27 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,9 @@
+// src/api/index.js
+import axios from "axios";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://axivibe.onrender.com/api",
-  withCredentials: true, // critical
+  baseURL: "https://api.nikhilmamdekar.site/api",
+  withCredentials: true,
 });
+
+export default api;
