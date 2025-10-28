9af83bc1d7ab739a88de90972d24fbcf0eb919c9
Halfwork9 <halfworks9@gmail.com>
2025-10-28 12:11:23 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,9 +1,24 @@
-// src/api/index.js
+// In your api/index.js file
 import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://api.nikhilmamdekar.site/api",
   withCredentials: true,
+  // Add this to ensure cookies are sent with all requests
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+// Add a request interceptor to include credentials in all requests
+api.interceptors.request.use(
+  (config) => {
+    config.withCredentials = true;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
