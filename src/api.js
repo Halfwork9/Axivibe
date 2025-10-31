b2316d93e5f18073f23a31cdd506c66fda56b340
Halfwork9 <halfworks9@gmail.com>
2025-10-31 12:22:05 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://api.nikhilmamdekar.site/api", // Use the full URL for production
-  timeout: 10000,
+  timeout: 30000,
   headers: {
     "Content-Type": "application/json",
   },
